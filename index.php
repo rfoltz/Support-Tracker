c3bfd1a1f69c931852f2b464bd3800cf3b4857a4
rfoltz <caboose512@hotmail.com>
2013-04-08 16:02:10 -0400

    Added style for the home and login pages.

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<!--Source File: index.php
+Name:Robert Foltz
+Last Modified By: Robert Foltz
+Website Name: Support Tracker
+File Description: This is the main page of the Support Track Ticket system.
+-->
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Support Tracker</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+		<!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+	<!--
+    Page Header and Menu
+    -->
+        <div class="header-container">
+            <header class="wrapper clearfix">
+            	<h1 id="logo" class="title">Support Tracker</h1>
+            	<nav class="login-menu">
+                    <ul>
+                    	<li><a href="index.php">Home</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+	<!--
+    Main Content
+    -->
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+                <article>
+                    <header>
+                        <h1>Welcome to Support Tracker</h1>
+                        <p>Support Tracker is a support ticket system packed into a website which is responsive so you're employees can view and update tickets on a variety of devices. There is also a mobile web app in the works as well which will allow your employees to view tickets effectively and efficiently on mobile devices.</p>
+                        <p>If you would like to try the demo please just login with with:</p>
+                        <p><label>Username: demo</label></p>
+                        <p><label>Password: 123456</label></p>
+                    </header>
+                </article>
+				
+				
+				
+				<!--
+                Follow Me / Social Networks
+                -->
+                <aside>
+                    <h3>Contact The Creator!</h3>
+                    <a href="//plus.google.com/109073700143949670409?prsrc=3" rel="publisher" style="text-decoration:none;">
+                    <img src="//ssl.gstatic.com/images/icons/gplus-32.png" alt="Google+" style="border:0;width:32px;height:32px;"/></a>
+                    <a href="//ca.linkedin.com/pub/robert-foltz/63/b61/593" rel="" style="text-decoration:none;">
+                    <img src="imgs/linkedin_32.png" alt="LinkedIn" style="border:0;width:32px;height:32px;"/></a>
+                    <p>Feel free to click on any of the following social networks and chat with me I'm always up for talking to new people.</p>
+                    <p>or email me at: <a class="link-colour" href="mailto:me@robertfoltz.com">me@robertfoltz.com</a></p>
+                </aside>
+
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+		
+        <!--
+        Page Footer
+        -->
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h5>Copyright of Robert Foltz 2013</h5>
+            </footer>
+        </div>
+        
+        <!--
+        Scripts/Script Linking
+        -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        
+        <!-- FlexSlider -->
+	    <script defer src="js/jquery.flexslider-min.js"></script>
+        
+        <!-- My Javascript. -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<!--Source File: index.php
+Name:Robert Foltz
+Last Modified By: Robert Foltz
+Website Name: Support Tracker
+File Description: This is the main page of the Support Track Ticket system.
+-->
+
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Support Tracker</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+		<!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+        
+
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    </head>
+    <body>
+	<!--
+    Page Header and Menu
+    -->
+        <div class="header-container">
+            <header class="wrapper clearfix">
+            	<h1 id="logo" class="title">Support Tracker</h1>
+            	<nav class="login-menu">
+                    <ul>
+                    	<li><a href="index.php">Home</a></li>
+                        <li><a href="login.php">Login</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+
+	<!--
+    Main Content
+    -->
+        <div class="main-container">
+            <div class="main wrapper clearfix">
+            	<h1>Support Tracker Login</h1>
+				<form action="#" method="POST" enctype="multipart/form-data" id="login_form">
+					<table id="login-table">
+						<tr>
+							<td>Username: </td>
+							<td><input type="text" name="username" id="username"></td>
+						</tr>
+						<tr>
+							<td>Password:</td>
+							<td><input type="password" name="password" id="password"></td>
+						</tr>
+						<tr>
+							<td colspan="2"><input id="submit" style="float:right;" type="submit" value="Login"></td>
+						</tr>
+					</table>
+				</form>
+            </div> <!-- #main -->
+        </div> <!-- #main-container -->
+
+		
+        <!--
+        Page Footer
+        -->
+        <div class="footer-container">
+            <footer class="wrapper">
+                <h5>Copyright of Robert Foltz 2013</h5>
+            </footer>
+        </div>
+        
+        <!--
+        Scripts/Script Linking
+        -->
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
+        
+        <!-- FlexSlider -->
+	    <script defer src="js/jquery.flexslider-min.js"></script>
+        
+        <!-- My Javascript. -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
